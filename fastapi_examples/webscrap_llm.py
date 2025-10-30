0e1bab7c3f33d1201de58c906a955e0fd25efc04
octaviorenteria-dev <octavio.renteria@cimat.mx>
2025-10-30 16:05:08 -0600

    ejemplo webscrap con llms
    Signed-off-by: octaviorenteria-dev <octavio.renteria@cimat.mx>

---
+++ fastapi_examples/webscrap_llm.py
@@ -0,0 +1,69 @@
+from fastapi import FastAPI, Path
+from typing import Annotated
+from pydantic import BaseModel
+from typing import List
+
+import logging
+import html2text
+import requests
+import asyncio
+import ollama
+
+
+app = FastAPI()
+
+
+logging.basicConfig(
+
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+
+)
+logger = logging.getLogger(__name__)
+
+class Quote(BaseModel):
+    name: str
+    text: str
+
+class Quotes(BaseModel):
+    quotes: List[Quote]
+
+
+
+async def server_loop():
+    h = html2text.HTML2Text()
+    prompt = '''
+    The following context contains a list of quotes. Extract the quotes in JSON format.
+    Each object in the list has the following properties:
+    Key "text": the text of the quote.
+    Key "name": the name of the author of the quote.
+    Return this list as a JSON object with the key "quotes".
+    CONTEXT:
+    '''
+    while True:
+        page = await app.queue_page.get()
+        r = requests.get(f'https://quotes.toscrape.com/page/{page}/')
+        text = h.handle(r.text)
+        
+        logger.info(text) 
+
+        response = await ollama_client.generate(model='gemma3:12b', prompt=prompt+text, format=Quotes.model_json_schema(), stream=False)
+
+        data = Quotes.model_validate_json(response['response'])
+
+        logger.info(data)    
+        await app.queue_results.put(data)
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "This page scraps data from the page https://quotes.toscrape.com"}
+
+@app.on_event("startup")
+def startup_event():    
+    ollama_client =  ollama.AsyncClient(host="http://localhost:11434")
+    app.queue_page = asyncio.Queue()
+    app.queue_results = asyncio.Queue()
+    app.task = asyncio.create_task(server_loop())
